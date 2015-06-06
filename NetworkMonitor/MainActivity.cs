92241da5492544eb40afc1ac785c28882b1c9d15
Arpit Singla <arpit270896@gmail.com>
2015-06-06 10:39:05 +0530

    got the name of subdirectories in log

--- NetworkMonitor/MainActivity.cs
+++ NetworkMonitor/MainActivity.cs
@@ -6,14 +6,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using System.IO;
 
 namespace NetworkMonitor
 {
 	[Activity (Label = "NetworkMonitor", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
+		
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
@@ -21,13 +22,13 @@ namespace NetworkMonitor
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			string dirpath = "/proc/uid_stat";
+			string[] subDirectories = Directory.GetDirectories(dirpath,"*");
+			foreach (string subDirectory in subDirectories) 
+			{
+				Log.Debug ("dirname", subDirectory);
+			}
+
 		}
 	}
 }
